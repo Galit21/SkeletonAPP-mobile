1114404f1768be4d7535397f118e4de7e3b4afba
Galit <dgallardo.aba@gmail.com>
2024-09-02 23:33:46 -0400

    Update index.page.ts

--- src/app/pages/index/index.page.ts
+++ src/app/pages/index/index.page.ts
@@ -7,13 +7,13 @@ import { Router } from '@angular/router';
   styleUrls: ['./index.page.scss'],
 })
 export class IndexPage implements OnInit {
-  user!: String;
+ /* user!: String;
   title: String = 'Bienvenido!';
 
   constructor(private router: Router) {
     const state = this.router.getCurrentNavigation()?.extras.state;
     this.user = state?.['user'] ?? 'defaultUser';
-  }
+  }*/
 
   ngOnInit() {}
 }
